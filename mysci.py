a6ab05907c3cc2fcae1467b2bdf9144152fe9b19
Jiaqi-beep <jiaqil@middlebury.edu>
2021-06-03 10:46:59 -0400

    compute wind chill factor

--- mysci.py
+++ mysci.py
@@ -1,8 +1,49 @@
-# Read the data file
+# Column names and column indices to read
+columns = {'date': 0, 'time': 1, 'tempout': 2, 'windspeed': 7}
+
+# Data types for each column (only if non-string)
+types = {'tempout': float, 'windspeed': float}
+
+# Initialize my data variable
+data = {}
+for column in columns:
+   data[column] = []
+
+# Read and parse the data file
 filename = "data/wxobs20170821.txt"
-#datafile = open(filename, 'r')
+with open(filename, 'r') as datafile:
 
-with open(filename, 'r') as datafile: # context manager that automatically closes when finished
-   data = datafile.read()
+   # Read the first three lines (header)
+   for _ in range(3):
+      datafile.readline()
 
-print(type(data))
+   # Read and parse the rest of the file
+   for line in datafile:
+      split_line = line.split()
+      for column in columns:
+         i = columns[column]
+         t = types.get(column, str) # avoiding key-not-found errors and defaulting to str if unspecified
+         value = t(split_line[i])
+         data[column].append(value)
+
+# DEBUG
+print(data['tempout'])
+
+
+# Compute the wind chill temperature
+def compute_windchill(t, v):
+	a = 35.74
+	b = 0.6215
+	c = 35.75
+	d = 0.4275
+
+	v2 = v ** 2
+    wci = a + (b * t) - (c * v2) + (d * t * v2)
+    return wci
+
+
+# Compute the wind chill factor
+windchill = []
+	# zip function in Python to automatically unravel the tuples
+for temp, windspeed in zip(data['tempout'], data['windspeed']):
+	windchill.append(compute_windchill(temp, windspeed))
