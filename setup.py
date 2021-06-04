bc1045f4c63d09a0bee9bc9d55a0316e8a93a86f
Jiaqi-beep <jiaqil@middlebury.edu>
2021-06-04 08:54:06 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package",
+    author="Jiaqi Li",
+    author_email="jiaqil@ucar.edu",
+    packages=["mysci"],
+    install_requires=[],
+)
